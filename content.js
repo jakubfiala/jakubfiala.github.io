58a47aba68201fa2a380a09bfa3c00a55634bc09
Jakub Fiala <j4kub.fiala@gmail.com>
2014-05-10 18:40:22 +0100

    Release

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     var mode = "#home";
 	var bio = "#bio";
 	var sound = "#sound";
-	var picture = "#picture";
+	var software = "#software";
 	var space = "#space";
 	var colors = ["#CC9009","#64FFEC","#FF8725","#09CC7D"];
 
@@ -25,62 +25,68 @@ $( document ).ready(function() {
 			      return { width : _pW, height : _pH };
 			    })();
 
+	$("body").width(dim.width);
+	$("#content").width(dim.width);
+	$("header").width(dim.width);
+
 
 	//CONTENT
-	var path = $.fn.scrollPath("getPath", {scrollSpeed: 20});
-	path.moveTo(dim.width/2,dim.height/2,{name: "_intro"});
-	path.lineTo(dim.width/2,$("#_bio").position().top+dim.height,{name: "_bio"});
-	path.lineTo(dim.width/2,$("#_sound").position().top+dim.height,{name: "_sound"});
-	path.lineTo(dim.width/2,$("#_picture").position().top+dim.height,{name: "_picture"});
-	path.lineTo(dim.width/2,$("#_space").position().top+dim.height,{name: "_space"});
-
-	//initialize scrollpath
-	$("#content").scrollPath({drawPath: false, wrapAround: false, scrollBar: false});
+	var items = ["#bio","#sound","#software","#space"];
+	var colours = ["#CC9009","#64FFEC","#FF8725","#09CC7D"];
 
 	//MENU----------------------------------------------------------------------------------
-	//$("#content").height(dim.width + "px");
 	$("#head").hover(function(){ $(this).css("cursor","default");}, function(){ $(this).css("cursor","default");});
 	$(".menuitem").hover(function(){ $(this).css("cursor","pointer");}, function(){ $(this).css("cursor","default");});
-	$("#bio").hover(function(){ $(this).css("color", "#CC9009");}, function(){ if (mode != bio) { $(this).css("color","#000");} });
-	$("#sound").hover(function(){ $(this).css("color", "#64FFEC");}, function(){ if (mode != sound) { $(this).css("color","#000");} });
-	$("#picture").hover(function(){ $(this).css("color", "#FF8725");}, function(){ if (mode != picture) { $(this).css("color","#000");} });
-	$("#space").hover(function(){ $(this).css("color", "#09CC7D");}, function(){ if (mode != space) { $(this).css("color","#000");} });
+	$("#home").hover(function(){ $(this).css("cursor","pointer");}, function(){ $(this).css("cursor","default");});
+	$(bio).hover(function(){ $(this).css("color", "#CC9009");}, function(){ if (mode != bio) { $(this).css("color","#000");} });
+	$(sound).hover(function(){ $(this).css("color", "#64FFEC");}, function(){ if (mode != sound) { $(this).css("color","#000");} });
+	$(software).hover(function(){ $(this).css("color", "#FF8725");}, function(){ if (mode != software) { $(this).css("color","#000");} });
+	$(space).hover(function(){ $(this).css("color", "#09CC7D");}, function(){ if (mode != space) { $(this).css("color","#000");} });
 
-	$("#bio").click(function(){ 
-		if (mode != bio) { 
-			$(mode).css("color","#000"); 
-			$(this).css("color", "#CC9009");
-			mode = bio;
-			$.fn.scrollPath("scrollTo","_bio");
+	$(".menuitem").each(function() {
+		var id = "#_" + $(this).attr("id");
+		console.log(id);
+		$(this).click(function(){ $("html, body").animate({ scrollTop: ($(id).offset().top-200) },500); });
+	})
+
+	$("#home").click(function() { $("html, body").animate({ scrollTop: 0 },500); });
+
+	$(document).scroll(function() {
+		if ($(document).scrollTop() < ($("#_bio").offset().top-200)) {
+			$(".menuitem").css("color", "#000000");
+			mode = "#home";
+		}
+		else {
+			if ($(document).scrollTop() < ($("#_sound").offset().top-200)) {
+				$(mode).css("color","#000000");
+				$(bio).css("color", "#CC9009");
+				mode = bio;
+			}
+			else {
+				if ($(document).scrollTop() < ($("#_software").offset().top-200)) {
+					$(mode).css("color","#000000");
+					$(sound).css("color", "#64FFEC");
+					mode = sound;
+				}
+				else {
+					if ($(document).scrollTop() < ($("#_space").offset().top-200)) {
+						$(mode).css("color","#000000");
+						$(software).css("color", "#FF8725");
+						mode = software;
+					}
+					else {
+						$(mode).css("color","#000000");
+						$(space).css("color", "#09CC7D");
+						mode = space;
+					}
+				}
+			}
 		}
 	});
 
-	$("#sound").click(function(){ 
-		if (mode != sound) { 
-			$(mode).css("color","#000"); 
-			$(this).css("color", "#64FFEC"); 
-			mode = sound;
-			$.fn.scrollPath("scrollTo","_sound");
-		} 
-	});
 
-	$("#picture").click(function(){ 
-		if (mode != picture) { 
-			$(mode).css("color","#000"); 
-			$(this).css("color", "#FF8725"); 
-			mode = picture;
-			$.fn.scrollPath("scrollTo","_picture");
-		} 
-	});
 
-	$("#space").click(function(){ 
-		if (mode != space) { 
-			$(mode).css("color","#000"); 
-			$(this).css("color", "#09CC7D"); 
-			mode = space;
-			$.fn.scrollPath("scrollTo","_space");
-		} 
-	});
+
 
 	//INTRO
 	var canvas = document.getElementById('animation');
@@ -89,7 +95,7 @@ $( document ).ready(function() {
 	var countY = 0;
 	var size = 50;
 	var forward = true;
-	canvas.height = dim.height - 50;
+	canvas.height = dim.height;
 	canvas.width = (Math.round(dim.width/(size*0.6)))*size*0.6 + size*0.6;
 
 	function newTriangle(x,y) {
@@ -110,8 +116,8 @@ $( document ).ready(function() {
 		posY = y*size;
 		posX = x*size*0.6;
 		ctx.beginPath();
-		ctx.moveTo(canvas.width-posX,canvas.height-posY+size/2+2);
-		ctx.lineTo(canvas.width-posX-size*0.6,canvas.height-posY+1);
+		ctx.moveTo(canvas.width-posX,canvas.height-posY+size/2);
+		ctx.lineTo(canvas.width-posX-size*0.6,canvas.height-posY);
 		ctx.lineTo(canvas.width-posX,canvas.height-posY-size/2);
 		ctx.lineTo(canvas.width-posX,canvas.height-posY+size/2);
 		ctx.closePath();
