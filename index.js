e7ace8c14f2170c0f99d8f98594c848ad20d3de1
jakubfiala <j4kub.fiala@gmail.com>
2026-01-31 14:13:35 +0100

    big design update (again lol)

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-import fragmentShaderSource from './shader.js';
-import { createRenderer } from './glsl-sandbox.js';
+import fragmentShaderSource from "./shader.js";
+import { createRenderer } from "./glsl-sandbox.js";
 
 const { canvas, draw } = createRenderer(fragmentShaderSource);
 
-const shouldHaveHeadStart = !document.documentElement.classList.contains('first-load')
-  || document.documentElement.classList.contains('subpage');
+const shouldHaveHeadStart =
+  !document.documentElement.classList.contains("first-load") ||
+  document.documentElement.classList.contains("subpage");
 
 const headStart = shouldHaveHeadStart ? 1000 : 0;
 
-canvas.id = 'bg';
+canvas.id = "bg";
 document.body.appendChild(canvas);
 
 const onWindowResize = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-}
+};
 
 onWindowResize();
-window.addEventListener('resize', onWindowResize, false);
+window.addEventListener("resize", onWindowResize, false);
 
 const mouse = new DOMPoint(0, 0);
 
-window.addEventListener('pointermove', (event) => {
+window.addEventListener("pointermove", (event) => {
   mouse.x = event.clientX / window.innerWidth;
   mouse.y = event.clientY / window.innerHeight;
 });
@@ -38,7 +39,14 @@ const animate = (time) => {
   }
 
   requestAnimationFrame(animate);
-  draw(time + headStart, renderedMouse.x, renderedMouse.y, canvas.width, canvas.height, false);
+  draw(
+    time + headStart,
+    renderedMouse.x,
+    renderedMouse.y,
+    canvas.width,
+    canvas.height,
+    false,
+  );
 };
 
 export const startAnimating = () => requestAnimationFrame(animate);
